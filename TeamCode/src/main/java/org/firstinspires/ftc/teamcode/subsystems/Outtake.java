30ad6072093e4f4a21216fa5c55f46a7da7b0fae
Ethan222 <ethannworks@gmail.com>
2024-02-28 17:39:47 -0500

    Update outtake servo positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -6,15 +6,15 @@ public class Outtake {
     public CustomServo flipper, extender, armRotator, pixelRotator, releaser;
     private final CustomServo[] servos;
     public Outtake(HardwareMap hardwareMap, String flipperName, String extenderName, String armRotatorName, String pixelRotatorName, String releaserName) {
-        flipper = new CustomServo("flipper", hardwareMap, flipperName, .6, .78);
+        flipper = new CustomServo("flipper", hardwareMap, flipperName, .59, .78);
         extender = new CustomServo("extender", hardwareMap, extenderName, .15, .69);
         armRotator = new CustomServo("armRotator", hardwareMap, armRotatorName, .33, .64);
         pixelRotator = new CustomServo("pixelRotator", hardwareMap, pixelRotatorName, 0, 1);
         releaser = new CustomServo("releaser", hardwareMap, releaserName, .1, .9);
         flipper.setPosition(flipper.getMinPos());
         extender.setPosition(extender.getMinPos());
-        armRotator.setPosition(.47);
-        pixelRotator.setPosition(.65);
+        armRotator.setPosition(.48);
+        pixelRotator.setPosition(.68);
         servos = new CustomServo[]{flipper, extender, armRotator, pixelRotator, releaser};
     }
     public String getTelemetry() {
