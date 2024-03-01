e595e6c7a728b3f8511867a0206765d1deed5bdc
Ethan222 <ethannworks@gmail.com>
2024-02-29 22:59:00 -0500

    Split drive init from robot init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
@@ -12,10 +12,17 @@ public class Robot {
     public Hang hang;
     public Launcher launcher;
     public Robot(HardwareMap hardwareMap, Pose2d startPose) {
-        drive = new MecanumDrive(hardwareMap, startPose);
+        this(hardwareMap);
+        initDrive(hardwareMap, startPose);
+    }
+    public Robot(HardwareMap hardwareMap) {
+        drive = null; // can't init drive w/o startPose
         intake = new Intake(hardwareMap, "intake", "");
         outtake = new Outtake(hardwareMap,"motor1","servo0","servo1","servo2","servo4","servo3");
         hang = new Hang(hardwareMap, "motor0");
         launcher = new Launcher(hardwareMap, "launcher");
+    }
+    public void initDrive(HardwareMap hardwareMap, Pose2d startPose) {
+        drive = new MecanumDrive(hardwareMap, startPose);
     }
 }
