3741605dd80cc48774f2b27c5c8e097553934703
Ethan222 <ethannworks@gmail.com>
2024-03-14 14:12:19 -0400

    updated to logistic arm equation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flipper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flipper.java
@@ -13,18 +13,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Config
 public class Flipper extends CustomServo {
     public static final String DOWN = "DOWN", UP = "UP";
-    public static double UP_POSITION = .87, DOWN_POSITION = .50, MAX_SPEED = .01, MIN_SPEED = .0005, SLOW_DOWN_POSITION = 0.63, INCREMENT = .001;
+    public static double UP_POSITION = .87, DOWN_POSITION = .50, SLOW_DOWN_POSITION = 0.63, INCREMENT = .001;
+    public static double MAX_SPEED = .02, MIN_SPEED = .0005, GROWTH_CONSTANT = 60;
+    private final double MIDPOINT = (UP_POSITION + DOWN_POSITION) / 2;
     public Flipper(HardwareMap hardwareMap, String id, double minPos, double maxPos) {
         super(hardwareMap, id, minPos, maxPos);
         setPosition(DOWN_POSITION);
     }
     public class Unflip implements Action {
-//        private final double DECAY_RATE = -Math.log(MIN_SPEED / MAX_SPEED) / (DOWN_POSITION - UP_POSITION);
+        private static double logisticGrowth(double L, double k, double x0, double x) {
+            return L / (1 + Math.exp(-k * (x - x0)));
+        }
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             double psn = getPosition();
-            double speed = Math.pow((psn - DOWN_POSITION) / (UP_POSITION - DOWN_POSITION), 1) * MAX_SPEED + MIN_SPEED;
-//            double speed = MAX_SPEED * Math.exp(-DECAY_RATE * (psn - UP_POSITION));
+            double speed = logisticGrowth(MAX_SPEED, GROWTH_CONSTANT, MIDPOINT, psn);
             telemetryPacket.put("speed", speed);
             if(psn > DOWN_POSITION) {
                 rotateBy(-speed);
